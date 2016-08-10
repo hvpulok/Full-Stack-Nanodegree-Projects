a27491f6048c2a5db59b585998cdc44af701e32d
hvpulok <hvpulok@gmail.com>
2016-08-10 12:21:08 -0600

    Add code to add dummy data

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -1,29 +1,46 @@
 -- code to create a tournament database
 DROP DATABASE IF EXISTS tournament;
+DROP TABLE IF EXISTS matches;
+DROP TABLE IF EXISTS players;
+
+
 CREATE DATABASE tournament;
 
--- Table definitions for the tournament project.
---
--- Put your SQL 'create table' statements in this file; also 'create view'
--- statements if you choose to use it.
---
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
-
 -- create players table
-DROP TABLE IF EXISTS players;
 CREATE TABLE players(                                                                                                                             
         playerID SERIAL PRIMARY KEY NOT NULL,                                                                                                             
-        NAME TEXT NOT NULL);
+        name TEXT NOT NULL);
 
 -- create matches table
-DROP TABLE IF EXISTS matches;
 CREATE TABLE matches(                                                                                                                             
         matchID SERIAL PRIMARY KEY NOT NULL,                                                                                                             
         winner INT NOT NULL REFERENCES players(playerID),
         loser INT NOT NULL REFERENCES players(playerID));
 
+-- sample players data
+-- ==========================
+
+INSERT INTO players (name) VALUES('Pulok');
+INSERT INTO players (name) VALUES('Anika');
+INSERT INTO players (name) VALUES('Akib');
+INSERT INTO players (name) VALUES('Rajib');
+INSERT INTO players (name) VALUES('Tony');
+INSERT INTO players (name) VALUES('Mike');
+INSERT INTO players (name) VALUES('Sara');
+INSERT INTO players (name) VALUES('Jeny');
+
+
+-- sample matches data
+-- ==========================
+INSERT INTO matches (winner, loser) VALUES(1, 2);
+INSERT INTO matches (winner, loser) VALUES(3, 4);
+INSERT INTO matches (winner, loser) VALUES(5, 6);
+INSERT INTO matches (winner, loser) VALUES(7, 8);
+
+
 
 SELECT * FROM players;
 SELECT * FROM matches;
 
+-- DELETE FROM players;
+-- DELETE FROM matches;
