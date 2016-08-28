f9ba99c6db57d186a235b9fea02cdfb179f8aa24
hvpulok <hvpulok@gmail.com>
2016-08-28 16:51:16 -0600

    updated dummy data with username info

--- vagrant/ItemCatalog_Project/seed_dB.py
+++ vagrant/ItemCatalog_Project/seed_dB.py
@@ -64,9 +64,9 @@ def createDummyUsers():
 
 def createDummySubjects():
     """Create dummy Subject"""
-    sub1 = Subject( user_id= 1, name="Web Development")
-    sub2 = Subject( user_id= 1, name="Mobile App Development")
-    sub3 = Subject( user_id= 2, name="Game Development")
+    sub1 = Subject( user_id= 1, user_name = "Admin" , name="Web Development")
+    sub2 = Subject( user_id= 2, user_name = "Mike", name="Mobile App Development")
+    sub3 = Subject( user_id= 3, user_name = "Sara", name="Game Development")
 
     session.add(sub1)
     session.add(sub2)
@@ -76,31 +76,31 @@ def createDummySubjects():
 
 def createDummyCourses():
     """Create dummy Courses"""
-    course1 = Course( user_id= 3, subject_id=1, name="Web Developer Bootcamp", 
+    course1 = Course( user_id= 3, author_name = "Mike", subject_id=1, name="Web Developer Bootcamp", 
                 description= "Lorem ipsum dolor sit amet, nunc erat voluptatem dapibus maecenas leo. Fusce placerat urna enim odio, ante leo ligula cras nam, mauris dolor gravida libero, proin aliquam velit duis gravida viverra ipsum, adipiscing habitant. Scelerisque nulla mollis nulla, fermentum non vel amet tellus mi, id posuere, a ipsum a metus amet leo, risus mi ullamcorper at facilisis etiam aliquam. Luctus ligula ligula metus. Ut pretium turpis donec vestibulum nulla, tellus arcu auctor morbi vitae, vitae ut ornare porttitor ut proin, urna mi elit sed dui euismod, enim sed vestibulum blandit. A sed dolor, tellus felis turpis, viverra voluptas commodo, malesuada vel mauris auctor, massa suspendisse fermentum.",
                 price = "$50")
-    course2 = Course( user_id= 2, subject_id=1,  name="Accelerated Java Script Training", 
+    course2 = Course( user_id= 2, author_name = "Sara", subject_id=1,  name="Accelerated Java Script Training", 
                 description= "Fermentum accumsan pede maecenas at vivamus vitae, varius esse nascetur blandit, aliquam montes dictum tristique odio, pellentesque sed fusce magna nulla, sodales est id libero. Lobortis in tellus dapibus massa ac, nisl luctus at nam nec eu, morbi cras porta vestibulum nulla posuere, aliquam malesuada. Mauris vel reiciendis tempus consectetuer egestas tellus, tempus faucibus sodales, tellus in elementum auctor habitasse, odio mauris, dis et at diam urna purus nunc. Id bibendum fermentum, et leo feugiat pellentesque metus ultricies, fringilla adipiscing, habitasse fringilla. In consectetuer aliquip erat parturient orci, integer pede risus quam quis porta. Scelerisque pharetra massa habitant sodales elementum, ornare nostra adipiscing.",
                 price = "$25")
-    course3 = Course( user_id= 1, subject_id=1,  name="HTML5 Training", 
+    course3 = Course( user_id= 1, author_name = "Admin", subject_id=1,  name="HTML5 Training", 
                 description= "Elit senectus consectetuer gravida ultricies sit magnis, nunc sed tincidunt, tempus amet montes sed aliquam pellentesque placerat. Eros aenean vel id turpis, feugiat lacus, lectus eget et. Dolor bibendum vitae augue placerat, tincidunt vel nibh pulvinar omnis feugiat et, adipiscing sed primis urna leo ad, ligula suspendisse vel. Hymenaeos ac vel turpis, luctus egestas sagittis molestie risus quis nunc, feugiat ornare fusce diam ut, et vitae fringilla iaculis quis. Ut sed nulla, vitae wisi ac aliquam vestibulum mauris, fusce malesuada dictum sit ultricies convallis rutrum. Sapien blandit massa sapien ligula a nulla, elementum ipsum volutpat id, sed diam erat, egestas eum sed dictumst tempor, tortor porttitor adipiscing ad ipsum temporibus.",
                 price = "$20")
-    course4 = Course( user_id= 3, subject_id=2, name="Mobile App Developer Bootcamp", 
+    course4 = Course( user_id= 3, author_name = "Sara", subject_id=2, name="Mobile App Developer Bootcamp", 
                 description= "Lorem ipsum dolor sit amet, nunc erat voluptatem dapibus maecenas leo. Fusce placerat urna enim odio, ante leo ligula cras nam, mauris dolor gravida libero, proin aliquam velit duis gravida viverra ipsum, adipiscing habitant. Scelerisque nulla mollis nulla, fermentum non vel amet tellus mi, id posuere, a ipsum a metus amet leo, risus mi ullamcorper at facilisis etiam aliquam. Luctus ligula ligula metus. Ut pretium turpis donec vestibulum nulla, tellus arcu auctor morbi vitae, vitae ut ornare porttitor ut proin, urna mi elit sed dui euismod, enim sed vestibulum blandit. A sed dolor, tellus felis turpis, viverra voluptas commodo, malesuada vel mauris auctor, massa suspendisse fermentum.",
                 price = "$50")
-    course5 = Course( user_id= 2, subject_id=2,  name="Learn iOS Developement", 
+    course5 = Course( user_id= 2, author_name = "Admin", subject_id=2,  name="Learn iOS Developement", 
                 description= "Fermentum accumsan pede maecenas at vivamus vitae, varius esse nascetur blandit, aliquam montes dictum tristique odio, pellentesque sed fusce magna nulla, sodales est id libero. Lobortis in tellus dapibus massa ac, nisl luctus at nam nec eu, morbi cras porta vestibulum nulla posuere, aliquam malesuada. Mauris vel reiciendis tempus consectetuer egestas tellus, tempus faucibus sodales, tellus in elementum auctor habitasse, odio mauris, dis et at diam urna purus nunc. Id bibendum fermentum, et leo feugiat pellentesque metus ultricies, fringilla adipiscing, habitasse fringilla. In consectetuer aliquip erat parturient orci, integer pede risus quam quis porta. Scelerisque pharetra massa habitant sodales elementum, ornare nostra adipiscing.",
                 price = "$25")
-    course6 = Course( user_id= 1, subject_id=2,  name="Mastering Android Developement", 
+    course6 = Course( user_id= 1, author_name = "Mike", subject_id=2,  name="Mastering Android Developement", 
                 description= "Elit senectus consectetuer gravida ultricies sit magnis, nunc sed tincidunt, tempus amet montes sed aliquam pellentesque placerat. Eros aenean vel id turpis, feugiat lacus, lectus eget et. Dolor bibendum vitae augue placerat, tincidunt vel nibh pulvinar omnis feugiat et, adipiscing sed primis urna leo ad, ligula suspendisse vel. Hymenaeos ac vel turpis, luctus egestas sagittis molestie risus quis nunc, feugiat ornare fusce diam ut, et vitae fringilla iaculis quis. Ut sed nulla, vitae wisi ac aliquam vestibulum mauris, fusce malesuada dictum sit ultricies convallis rutrum. Sapien blandit massa sapien ligula a nulla, elementum ipsum volutpat id, sed diam erat, egestas eum sed dictumst tempor, tortor porttitor adipiscing ad ipsum temporibus.",
                 price = "$20")
-    course7 = Course( user_id= 3, subject_id=3, name="Game Developer Bootcamp", 
+    course7 = Course( user_id= 3, author_name = "Admin", subject_id=3, name="Game Developer Bootcamp", 
                 description= "Lorem ipsum dolor sit amet, nunc erat voluptatem dapibus maecenas leo. Fusce placerat urna enim odio, ante leo ligula cras nam, mauris dolor gravida libero, proin aliquam velit duis gravida viverra ipsum, adipiscing habitant. Scelerisque nulla mollis nulla, fermentum non vel amet tellus mi, id posuere, a ipsum a metus amet leo, risus mi ullamcorper at facilisis etiam aliquam. Luctus ligula ligula metus. Ut pretium turpis donec vestibulum nulla, tellus arcu auctor morbi vitae, vitae ut ornare porttitor ut proin, urna mi elit sed dui euismod, enim sed vestibulum blandit. A sed dolor, tellus felis turpis, viverra voluptas commodo, malesuada vel mauris auctor, massa suspendisse fermentum.",
                 price = "$50")
-    course8 = Course( user_id= 2, subject_id=3,  name="Learn by building a working Game", 
+    course8 = Course( user_id= 2, author_name = "Mike", subject_id=3,  name="Learn by building a working Game", 
                 description= "Fermentum accumsan pede maecenas at vivamus vitae, varius esse nascetur blandit, aliquam montes dictum tristique odio, pellentesque sed fusce magna nulla, sodales est id libero. Lobortis in tellus dapibus massa ac, nisl luctus at nam nec eu, morbi cras porta vestibulum nulla posuere, aliquam malesuada. Mauris vel reiciendis tempus consectetuer egestas tellus, tempus faucibus sodales, tellus in elementum auctor habitasse, odio mauris, dis et at diam urna purus nunc. Id bibendum fermentum, et leo feugiat pellentesque metus ultricies, fringilla adipiscing, habitasse fringilla. In consectetuer aliquip erat parturient orci, integer pede risus quam quis porta. Scelerisque pharetra massa habitant sodales elementum, ornare nostra adipiscing.",
                 price = "$25")
-    course9 = Course( user_id= 1, subject_id=3,  name="Become A Game Developer", 
+    course9 = Course( user_id= 1, author_name = "Sara", subject_id=3,  name="Become A Game Developer", 
                 description= "Elit senectus consectetuer gravida ultricies sit magnis, nunc sed tincidunt, tempus amet montes sed aliquam pellentesque placerat. Eros aenean vel id turpis, feugiat lacus, lectus eget et. Dolor bibendum vitae augue placerat, tincidunt vel nibh pulvinar omnis feugiat et, adipiscing sed primis urna leo ad, ligula suspendisse vel. Hymenaeos ac vel turpis, luctus egestas sagittis molestie risus quis nunc, feugiat ornare fusce diam ut, et vitae fringilla iaculis quis. Ut sed nulla, vitae wisi ac aliquam vestibulum mauris, fusce malesuada dictum sit ultricies convallis rutrum. Sapien blandit massa sapien ligula a nulla, elementum ipsum volutpat id, sed diam erat, egestas eum sed dictumst tempor, tortor porttitor adipiscing ad ipsum temporibus.",
                 price = "$20")
 
