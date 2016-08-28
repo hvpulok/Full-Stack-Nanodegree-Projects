a686992680c63ffd53bfb0830992b6075896c65e
hvpulok <hvpulok@gmail.com>
2016-08-28 11:58:04 -0600

    Define subjects DB schema

--- vagrant/ItemCatalog_Project/database_setup.py
+++ vagrant/ItemCatalog_Project/database_setup.py
@@ -1,4 +1,4 @@
-# This file setup course catalog database definitions and schemas
+# This file setup ecourse catalog database definitions and schemas
 
 import os
 import sys
@@ -19,7 +19,23 @@ class User(Base):
     email = Column(String(250), nullable=False)
     picture = Column(String(250))
 
+class subjects(Base):
+    """ This clas defines subjects dB schema """
+    __tablename__ = 'subjects'
+   
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
+    @property
+    def serialize(self):
+       """Return subjects object data in easily serializeable format"""
+       return {
+            'name'         : self.name,
+            'id'           : self.id,
+            'user_id'      : self.user_id,           
+       }
 
 # definition to create database engine
 engine = create_engine('sqlite:///course_catalog.db')
