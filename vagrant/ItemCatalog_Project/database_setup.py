0ecbb72e4e0ad6f232061d4f3f07b530f5cb1445
hvpulok <hvpulok@gmail.com>
2016-08-28 12:10:24 -0600

    define Course dB Schema

--- vagrant/ItemCatalog_Project/database_setup.py
+++ vagrant/ItemCatalog_Project/database_setup.py
@@ -19,22 +19,53 @@ class User(Base):
     email = Column(String(250), nullable=False)
     picture = Column(String(250))
 
-class subjects(Base):
+
+class Subject(Base):
     """ This clas defines subjects dB schema """
-    __tablename__ = 'subjects'
+    __tablename__ = 'subject'
    
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     user_id = Column(Integer, ForeignKey('user.id'))
+    user_name = Column(Integer, ForeignKey('user.name'))
     user = relationship(User)
 
     @property
     def serialize(self):
        """Return subjects object data in easily serializeable format"""
        return {
-            'name'         : self.name,
-            'id'           : self.id,
-            'user_id'      : self.user_id,           
+            'id'                    : self.id,
+            'name'                  : self.name,
+            'user_id'               : self.user_id,           
+            'user_name'             : self.user_name,           
+       }
+
+
+class Course(Base):
+    __tablename__ = 'course'
+
+    id = Column(Integer, primary_key = True)
+    name =Column(String(80), nullable = False)
+    description = Column(String(10000))
+    price = Column(String(8))
+    subject_id = Column(Integer,ForeignKey('subject.id'))
+    subject = relationship(subject)
+    author_id = Column(Integer, ForeignKey('user.id'))
+    author_name = Column(Integer, ForeignKey('user.name'))
+    user = relationship(User)
+
+
+    @property
+    def serialize(self):
+       """Return Course object data in easily serializeable format"""
+       return {
+            'id'                    : self.id,
+            'name'                  : self.name,
+            'description'           : self.description,
+            'price'                 : self.price,
+            'subject'               : self.subject,
+            'author_id'             : self.author_id,    
+            'author_name'           : self.author_name,    
        }
 
 # definition to create database engine
